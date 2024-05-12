371df4470d15b2db68bda08c9c19e1619ecb7617
Mr_h <hoangmn0902@gmail.com>
2024-05-12 19:25:38 +0700

    Update build files

--- wp-content/plugins/hoang-advanced-search/build/index.asset.php
+++ wp-content/plugins/hoang-advanced-search/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e53d8fce5f14fd5c39ad');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6c830f2aee54306d7a3d');
